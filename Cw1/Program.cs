71b7d66bb3e957b9ebfdd25fcf768f0c162e2f40
BuildTools <unconfigured@null.spigotmc.org>
2020-03-08 22:42:45 +0100

    Added new features

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,6 +13,12 @@ namespace Cw1
         {
             var newPerson = new Person { FirstName = "Marcin" };
 
+            if (args.Length == 0) throw new ArgumentNullException();
+            var pattern = new Regex("^http(s)?://([\\w-]+.)+[\\w-]+(/[\\w- ./?%&=])?$");
+            if (!pattern.IsMatch(args[0])) throw new ArgumentException();
+            
+            
+            
             var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
             
             Console.WriteLine("");
@@ -19,20 +27,33 @@ namespace Cw1
 
             using (var Client = new HttpClient())
             {
-                using (var response = await Client.GetAsync(url)){ 
-                if (response.IsSuccessStatusCode)
+                try
                 {
-                    var htmlContent = await response.Content.ReadAsStringAsync();
-                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
-
-                    var matches = regex.Matches(htmlContent);
-
-                    foreach (var match in matches)
+                    using (var response = await Client.GetAsync(url))
                     {
-                        Console.WriteLine(match);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var htmlContent = await response.Content.ReadAsStringAsync();
+                            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                            var uniqueMatches =regex.Matches(htmlContent)
+                                .OfType<Match>()
+                                .Select(m => m.Value)
+                                .Distinct();
+                            if (uniqueMatches.Count() == 0) Console.WriteLine("Nie znaleziono adresow email");
+
+                            foreach (var match in uniqueMatches)
+                            {    
+                                Console.WriteLine(match);
+                            }
+                        }
                     }
                 }
-            }
+                catch(HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Blad w czasie pobierania strony");
+                }
             }
 
           
